e1b5909337e8f69f4cf23b8fee19b55be3d09361
Ryan O'Connor <rocifier@gmail.com>
2014-11-24 02:18:12 +1300

    finished tabs

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -3,22 +3,30 @@ class TabsController < ApplicationController
     @tabs = Tabs.all
   end
   def show
-    @tab = Tabs.find_by name: params['name']
+    @tab = Tabs.find(params['id'])     
+    render partial: 'tab'
   end
   def create
     tab = Tabs.new
     tab.name = params['tabs']['name']
     tab.ajax_content = params['tabs']['ajax_content']
-    tab.save! 
+    tab.save!
+    redirect_to tabs_path 
   end
   def new
   end
   def edit
+    @tab = Tabs.find(params['id'])
   end 
   def update
+    tab = Tabs.find(params['id'])
+    tab.name = params['tabs']['name']
+    tab.ajax_content = params['tabs']['ajax_content']
+    tab.save!
+    redirect_to tabs_path
   end 
   def destroy
-    tab = Tabs.find_by(:name => params['tabs']['name'])
+    tab = Tabs.find(params['id'])
     tab.destroy if tab
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   get 'tabs'            => 'tabs#index', :as => :tabs
   post 'tabs'           => 'tabs#create', :as => :create_tab
   get 'tabs/new'        => 'tabs#new', :as => :new_tab
-  get 'tabs/:name/edit' => 'tabs#edit', :as => :edit_tab
-  get 'tabs/:name'      => 'tabs#show', :as => :show_tab
-  put 'tabs/:name'      => 'tabs#update', :as => :update_tab
-  delete 'tabs/:name'   => 'tabs#destroy', :as => :destroy_tab
+  get 'tabs/:id/edit'   => 'tabs#edit', :as => :edit_tab
+  get 'tabs/:id'        => 'tabs#show', :as => :show_tab
+  put 'tabs/:id'        => 'tabs#update', :as => :update_tab
+  delete 'tabs/:id'     => 'tabs#destroy', :as => :destroy_tab
   
 
   devise_for :users, :controllers => {omniauth_callbacks: 'omniauth_callbacks'}
